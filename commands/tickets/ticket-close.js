4b0c69ecb29a7a5ddf6faad5875a6c3f860406c5
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2021-12-26 18:22:01 +0100

    fix(structure): change ticket commands structure

---
+++ commands/tickets/ticket-close.js
@@ -0,0 +1,48 @@
+const BaseCommand = require('../../utils/structures/BaseCommand')
+const { Permissions, MessageEmbed} = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Ticket } = require('../../src/schemas/TicketSchema');
+const {sleep} = require("../../functions/utilitaryFunctions");
+
+module.exports = class TicketCloseCommand extends BaseCommand {
+    constructor() {
+        super('ticket-close', 'tickets', {
+            usage: "ticket-close",
+            description: `Close this ticket`,
+            categoryDisplayName: '✉ Tickets'
+        }, {
+            clientPermissions: [Permissions.FLAGS.MANAGE_CHANNELS],
+            userPermissions: [Permissions.FLAGS.MANAGE_ROLES],
+            home: true
+        });
+
+        this.builder = new SlashCommandBuilder()
+            .setDefaultPermission(true)
+            .setName(this.name)
+            .setDescription(this.help.description)
+    }
+    async run(client, interaction, options) {
+        const ticket = await Ticket?.findOne({ archive: false, ticketChannelId: interaction.channel.id });
+        if (!ticket) return client.replyError(interaction, 'This channel is not a ticket');
+
+        let deleteEmbed = new MessageEmbed()
+            .setDescription("Deleting ticket in 5 seconds")
+            .setColor('#ff5733')
+        interaction.reply({
+            embeds: [deleteEmbed]
+        })
+
+        await sleep(5000);
+        try {
+            ticket.archive = true;
+            await ticket.save();
+            interaction.channel.delete();
+            this.log('A ticket was deleted', {
+                ticketName: ticket.name,
+                operator: interaction.user.username
+            })
+        } catch (err) {
+            this.error("An error occurred while trying to delete a ticket", err);
+        }
+    }
+}
---
+++ commands/tickets/ticket-exit.js
@@ -0,0 +1,43 @@
+const BaseCommand = require('../../utils/structures/BaseCommand')
+const { Permissions, MessageEmbed} = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Ticket } = require('../../src/schemas/TicketSchema');
+
+module.exports = class TicketExitCommand extends BaseCommand {
+    constructor() {
+        super('ticket-exit', 'tickets', {
+            usage: "ticket-exit",
+            description: `Leave the ticket`,
+            categoryDisplayName: '✉ Tickets'
+        }, {
+            clientPermissions: [Permissions.FLAGS.MANAGE_CHANNELS],
+            userPermissions: [Permissions.FLAGS.MANAGE_ROLES],
+            home: true
+        });
+
+        this.builder = new SlashCommandBuilder()
+            .setDefaultPermission(true)
+            .setName(this.name)
+            .setDescription(this.help.description)
+    }
+    async run(client, interaction, options) {
+        const ticket = await Ticket?.findOne({ archive: false, ticketChannelId: interaction.channel.id });
+        if (!ticket) return client.replyError(interaction, 'This channel is not a ticket');
+
+        let leaveEmbed = new MessageEmbed()
+            .setDescription(`\`${interaction.author.username}\` just left the ticket 👋`)
+            .setColor('#f39c12')
+        try {
+            interaction.channel.permissionOverwrites.delete(interaction.user.id)
+            interaction.channel.send({
+                embeds: [leaveEmbed]
+            })
+            this.log("User left a ticket", {
+                user: interaction.user.username,
+                ticketName: ticket.name
+            })
+        } catch (err) {
+            this.error("An error occurred while trying to leave a ticket", err);
+        }
+    }
+}
