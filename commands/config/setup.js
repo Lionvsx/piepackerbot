f750bb0e6d75259ff5f03bba3c61737a3508e53e
Leonard Roussard <lroussard@gmail.com>
2022-01-25 13:49:46 +0100

    feat: add setup cmd

--- commands/config/setup.js
+++ commands/config/setup.js
@@ -20,11 +20,30 @@ module.exports = class SetupCommand extends BaseCommand {
             .setDescription(this.help.description)
     }
     async run(client, interaction, options) {
-        
-        //Create Reaction Role
+
         //Create Ticket Opening Embed
         //Create Auto Voice channels??
         //Change Bot activity
+        //Setup important channel links
+
+        let logChannelString =
+
+        const menuEmbed = new MessageEmbed()
+            .setTitle("Piepacker Bot setup menu")
+            .setDescription("Setup the different channels and roles required in order for the bot to work properly")
+            .setColor("#5a46b7")
+            .addFields([
+                {name: "Log Channel", value: logChannelString, inline: true},
+                {name: "Ticket Request Channel", value: `\`\`\`qsdqsd\`\`\``, inline: true},
+                {name: "FR Role", value: `\`\`\`qsdqsd\`\`\``, inline: true},
+                {name: "EN Role", value: `\`\`\`qsdqsd\`\`\``, inline: true},
+                { name: "ES Role", value: "", inline: true}
+            ])
+
+        //Channels :
+        //LogChannelId
+        //TicketRequest ChannelId
+        //
 
     }
 }
