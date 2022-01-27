2c1ad5793d9854ed6f20ffea8402e4a3d186847a
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2022-01-27 14:15:08 +0100

    feat(command): embed language full support

--- commands/config/embed-language.js
+++ commands/config/embed-language.js
@@ -1,13 +1,13 @@
 const BaseCommand = require('../../utils/structures/BaseCommand')
 const { MessageEmbed, Permissions, MessageActionRow} = require("discord.js");
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const {createButton} = require("../../functions/messageComponents");
+const {createEmojiButton} = require("../../functions/messageComponents");
 
 module.exports = class EmbedLanguage extends BaseCommand {
     constructor() {
         super('embed-language', 'config', {
             usage: 'embed-language',
-            description: '',
+            description: 'Sends language embed',
             categoryDisplayName: '⚙️ Config'
         }, {
             clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
@@ -22,18 +22,23 @@ module.exports = class EmbedLanguage extends BaseCommand {
     }
     async run(client, interaction, options) {
         const embed = new MessageEmbed()
-            .setTitle("")
-            .setDescription("")
+            .setTitle("Please select a language")
+            .setDescription("You can select multiple languages. To get the language role, just click the button, clicking the button as you already have the role will remove it from your roles.")
+            .setColor("#50e0e0")
 
-        await interaction.deferUpdate();
+        await client.replyLoading(interaction, "Sending embed...")
 
         interaction.channel.send({
             embeds: [embed],
             components: [new MessageActionRow().addComponents([
-                createButton(`accessChannel|`, "FR", "PRIMARY", "🇫🇷"),
-                createButton(`accessChannel|`, "ENG", "PRIMARY", "🇬🇧")
+                createEmojiButton(`buttonRole|935524858924773416`, "Français", "PRIMARY", "🇫🇷"),
+                createEmojiButton(`buttonRole|935524818269401108`, "English", "PRIMARY", "🇬🇧"),
+                createEmojiButton(`buttonRole|935524882245115914`, "Español", "PRIMARY", "🇪🇸"),
+                createEmojiButton(`buttonRole|935524921554108426`, "Português", "PRIMARY", "🇧🇷"),
             ])]
         })
+
+        await interaction.deleteReply();
     }
 }
 
