8e647df4178da9a25f0fb2a1d04dc2a7b27375c7
Leonard Roussard <lroussard@gmail.com>
2022-01-28 18:22:28 +0100

    fix(client): add dmchannel support

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-const { Client, Intents, Options, Interaction, Message, TextChannel } = require('discord.js');
+const { Client, Intents, Options, Interaction, Message, TextChannel, DMChannel} = require('discord.js');
 require('dotenv').config();
 const Logger = require('../utils/services/logger')
 
@@ -64,24 +64,28 @@ class client extends Client {
         if (object instanceof Interaction) return object.reply({content: `**${this.successEmoji} | **${content}`})
         if (object instanceof Message) return object.reply(`**${this.successEmoji} | **${content}`)
         if (object instanceof TextChannel) return object.send(`**${this.successEmoji} | **${content}`)
+        if (object instanceof DMChannel) return object.send(`**${this.successEmoji} | **${content}`)
     }
 
     replyError(object, content) {
         if (object instanceof Interaction) return object.reply({content: `**${this.errorEmoji} | **${content}`, ephemeral: true})
         if (object instanceof Message) return object.reply(`**${this.errorEmoji} | **${content}`)
         if (object instanceof TextChannel) return object.send(`**${this.errorEmoji} | **${content}`)
+        if (object instanceof DMChannel) return object.send(`**${this.errorEmoji} | **${content}`)
     }
 
     replyWarning(object, content) {
         if (object instanceof Interaction) return object.reply({content: `**${this.warningEmoji} | **${content}`, ephemeral: true})
         if (object instanceof Message) return object.reply(`**${this.warningEmoji} | **${content}`)
         if (object instanceof TextChannel) return object.send(`**${this.warningEmoji} | **${content}`)
+        if (object instanceof DMChannel) return object.send(`**${this.warningEmoji} | **${content}`)
     }
 
     replyLoading(object, content) {
         if (object instanceof Interaction) return object.reply({content: `**${this.loadingEmoji} | **${content}`})
         if (object instanceof Message) return object.reply(`**${this.loadingEmoji} | **${content}`)
         if (object instanceof TextChannel) return object.send(`**${this.loadingEmoji} | **${content}`)
+        if (object instanceof DMChannel) return object.send(`**${this.loadingEmoji} | **${content}`)
     }
 
 
