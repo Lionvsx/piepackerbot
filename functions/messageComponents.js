583548870327dde33e479346ee6dc03f43476b81
Leonard Roussard <lroussard@gmail.com>
2022-01-29 19:06:44 +0100

    feat(functions): add message components functions

--- functions/messageComponents.js
+++ functions/messageComponents.js
@@ -1,8 +1,12 @@
-const {MessageButton, MessageActionRow, GuildEmoji, ReactionEmoji} = require("discord.js");
+const {MessageButton, MessageActionRow, GuildEmoji, ReactionEmoji, MessageSelectMenu} = require("discord.js");
 module.exports = {
     createButton,
     createEmojiActionRow,
-    createEmojiButton
+    createEmojiButton,
+    createSelectionMenu,
+    createSelectionMenuOption,
+    getMenuOption,
+    setDefaultOption
 }
 
 /**
@@ -50,3 +54,47 @@ function createEmojiActionRow(emojiArray) {
     return emojiActionRow
 }
 
+/**
+ *
+ * @param {String} menuId
+ * @param {String} placeholderText
+ * @param {Object[]} menuOptionArray
+ * @param {Number} minSelections
+ * @param {Number} maxSelections
+ * @returns {MessageSelectMenu}
+ */
+function createSelectionMenu(menuId, placeholderText, menuOptionArray, minSelections, maxSelections) {
+    return new MessageSelectMenu()
+        .setCustomId(menuId)
+        .setPlaceholder(placeholderText)
+        .addOptions(menuOptionArray)
+        .setMinValues(minSelections)
+        .setMaxValues(maxSelections)
+}
+
+function createSelectionMenuOption(optionId, displayText, optDescription, optEmoji, optIsDefaultOption) {
+    return {
+        label: displayText,
+        value: optionId,
+        description: optDescription ?  optDescription : undefined,
+        emoji: optEmoji ? optEmoji : undefined,
+        default: optIsDefaultOption ? optIsDefaultOption : undefined
+    }
+}
+
+function getMenuOption(menuComponent, optionValue) {
+    const selectedOption = menuComponent.options.find(option => option.value === optionValue)
+    const optionIndex = menuComponent.options.indexOf(selectedOption)
+
+    return menuComponent.options[optionIndex]
+}
+
+function setDefaultOption(menuComponent, optionValue) {
+    const selectedOption = menuComponent.options.find(option => option.value === optionValue)
+    const optionIndex = menuComponent.options.indexOf(selectedOption)
+
+    for (const option of menuComponent.options) {
+        option.default = false;
+    }
+    menuComponent.options[optionIndex].default = true;
+}
